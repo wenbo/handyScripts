52b2a6e87333b527db174c38b97aef7a048dd2b3
wenbo <yiyun6674@gmail.com>
2012-04-19 14:40:18 +0800

    :scriptnames

--- cool.sh
+++ cool.sh
@@ -1,2 +1,5 @@
 cp spec/api/requests_spec.rb{,bk}
 sudo chown -R `whoami` /var/log/cassandra 
+linux-2.6.32.59$ find -name *defconfig* 
+linux-2.6.32.59$ grep ".arch.info.init" * -nR
+$su - #  (注意有- ，这和su是不同的，在用命令"su"的时候只是切换到root，但没有把root的环境变量传过去
