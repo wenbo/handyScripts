58bceb54ae5fe8c0af428f00fbb47f6ac672389b
wenbo <yiyun6674@gmail.com>
2012-04-23 14:57:01 +0800

    added a command line 'pmap pid'

--- cool.sh
+++ cool.sh
@@ -3,3 +3,5 @@ sudo chown -R `whoami` /var/log/cassandra
 linux-2.6.32.59$ find -name *defconfig* 
 linux-2.6.32.59$ grep ".arch.info.init" * -nR
 $su - #  (注意有- ，这和su是不同的，在用命令"su"的时候只是切换到root，但没有把root的环境变量传过去
+~$ netstat -nap |grep 7001
+pmap pid
