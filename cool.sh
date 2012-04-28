60d3107647f417e77cdec9cc07caf3e38b21e0d2
wenbo <yiyun6674@gmail.com>
2012-04-28 10:40:20 +0800

    sudo tcpdump -i eth0

--- cool.sh
+++ cool.sh
@@ -5,3 +5,8 @@ linux-2.6.32.59$ grep ".arch.info.init" * -nR
 $su - #  (注意有- ，这和su是不同的，在用命令"su"的时候只是切换到root，但没有把root的环境变量传过去
 ~$ netstat -nap |grep 7001
 pmap pid
+ssh -qTfnN -D 7070 wenbo@0371zhong.com
+ps -ef |grep  httpd| grep -v grep| cut -c 9-15| sudo xargs kill -9
+wenbo@hp:mod_perl-2.0.6$ perl Makefile.PL MP_APXS=/usr/local/apache2/bin/apxs
+MP_APR_CONFIG=/usr/local/apr/bin/apr-1-config #wenbo@hp:httpd-2.2.9
+sudo tcpdump -i eth0
