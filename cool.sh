8b1662d36fe24198aa39592d8d85891302eadce3
wenbo <yiyun6674@gmail.com>
2012-05-05 22:45:58 +0800

    just made a note about steps to install lxr on server

--- cool.sh
+++ cool.sh
@@ -7,8 +7,16 @@ $su - #  (注意有- ，这和su是不同的，在用命令"su"的时候只是�
 pmap pid
 ssh -qTfnN -D 7070 wenbo@0371zhong.com
 ps -ef |grep  httpd| grep -v grep| cut -c 9-15| sudo xargs kill -9
+
+################# LXR @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+./configure --prefix=/usr/local/apache2
+LoadModule perl_module modules/mod_perl.so
+cpan -l | grep File::MMagic
+/etc/postgresql/9.1/main
 wenbo@hp:mod_perl-2.0.6$ perl Makefile.PL MP_APXS=/usr/local/apache2/bin/apxs
 MP_APR_CONFIG=/usr/local/apr/bin/apr-1-config #wenbo@hp:httpd-2.2.9
+################# LXR @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 sudo tcpdump -i eth0
 sudo tcpdump -i eth0 'tcp port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) -
  ((tcp[12]&0xf0)>>2)) != 0)'
--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ source ~/.bash_profile
 
 sudo /usr/bin/apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
 
+sudo apt-get install libpq-dev 
 sudo apt-get install fglrx-updates fglrx-amdcccle-updates
 
 sudo apt-get install libmysql-ruby libmysqlclient-dev
