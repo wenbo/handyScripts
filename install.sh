2fe49baadccea730f6e574f76a203b642c94419e
wenbo <wenbo@wenbo-HP-Pavilion-g4-Notebook-PC.(none)>
2012-05-25 23:35:56 +0800

    xrandr --output VGA1 --auto --output LVDS1 --off

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ sudo apt-get install git
 
 sudo apt-get install curl
 
-bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscr)
+curl -L get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
 source ~/.bash_profile
 
 sudo /usr/bin/apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
@@ -27,3 +28,6 @@ for x in  `ps -eo pid,comm|grep scim|awk -F\  '{ print $1 }'`; do echo $x&& kill
 
 dpkg --get-selections > myselections
 dpkg --set-selections < myselections
+
+打开外接显示器，同时关闭笔记本液晶屏幕（只用外接显示器工作）
+xrandr --output VGA1 --auto --output LVDS1 --off
