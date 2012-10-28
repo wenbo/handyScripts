547b88b933abb541bd51438a76c7a973a46b54ea
wen_bo <wen_bo@kembo-net.com>
2012-10-28 22:56:42 +0800

    added logrotate

--- cool.sh
+++ cool.sh
@@ -3,6 +3,7 @@ sudo chown -R `whoami` /var/log/cassandra
 linux-2.6.32.59$ find -name *defconfig* 
 linux-2.6.32.59$ grep ".arch.info.init" * -nR
 $su - #  (注意有- ，这和su是不同的，在用命令"su"的时候只是切换到root，但没有把root的环境变量传过去
+type cd # cd is a shell builtin
 ~$ netstat -nap |grep 7001
 Firefox plugin: autoproxy
 pmap pid
