ff216ee87715a485bf10b31ae65f4d71fd4fa206
wen_bo <wen_bo@kembo-net.com>
2013-01-27 14:10:48 +0800

    added postfix

---
+++ mailserver/maildb.sql
@@ -0,0 +1,174 @@
+-- MySQL dump 10.13  Distrib 5.5.24, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: maildb
+-- ------------------------------------------------------
+-- Server version	5.5.24-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Temporary table structure for view `view_aliases`
+--
+
+DROP TABLE IF EXISTS `view_aliases`;
+/*!50001 DROP VIEW IF EXISTS `view_aliases`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `view_aliases` (
+  `email` varchar(91),
+  `destination` varchar(80)
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary table structure for view `view_users`
+--
+
+DROP TABLE IF EXISTS `view_users`;
+/*!50001 DROP VIEW IF EXISTS `view_users`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `view_users` (
+  `email` varchar(91),
+  `password` varchar(32)
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `virtual_aliases`
+--
+
+DROP TABLE IF EXISTS `virtual_aliases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_aliases` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain_id` int(11) NOT NULL,
+  `source` varchar(40) NOT NULL,
+  `destination` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `domain_id` (`domain_id`),
+  CONSTRAINT `virtual_aliases_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_aliases`
+--
+
+LOCK TABLES `virtual_aliases` WRITE;
+/*!40000 ALTER TABLE `virtual_aliases` DISABLE KEYS */;
+INSERT INTO `virtual_aliases` VALUES (1,2,'tangseng','tang.sanzang@gmail.com'),(2,1,'bajie','bajie@mytest.com'),(3,1,'bajie','zhu_bajie@yahoo.com'),(4,1,'','spams@mytest.com');
+/*!40000 ALTER TABLE `virtual_aliases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_domains`
+--
+
+DROP TABLE IF EXISTS `virtual_domains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_domains` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_domains`
+--
+
+LOCK TABLES `virtual_domains` WRITE;
+/*!40000 ALTER TABLE `virtual_domains` DISABLE KEYS */;
+INSERT INTO `virtual_domains` VALUES (1,'mytest.com'),(2,'dongyouji.cn');
+/*!40000 ALTER TABLE `virtual_domains` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_users`
+--
+
+DROP TABLE IF EXISTS `virtual_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain_id` int(11) NOT NULL,
+  `user` varchar(40) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `quota` int(10) DEFAULT '102400',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE_EMAIL` (`domain_id`,`user`),
+  CONSTRAINT `virtual_users_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_users`
+--
+
+LOCK TABLES `virtual_users` WRITE;
+/*!40000 ALTER TABLE `virtual_users` DISABLE KEYS */;
+INSERT INTO `virtual_users` VALUES (1,1,'bajie','00e28675230f4c9b16666098941e5d6d',10240),(2,1,'wukong','407dbf9a4ca5a9906332ff0ea9c330fb',102400),(3,2,'tangseng','3dbc34bca13af30ed7aa2769ee468b40',1048576),(4,1,'spams','946bfb68686ed81aa5b0c58bb2633175',1024);
+/*!40000 ALTER TABLE `virtual_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `view_aliases`
+--
+
+/*!50001 DROP TABLE IF EXISTS `view_aliases`*/;
+/*!50001 DROP VIEW IF EXISTS `view_aliases`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `view_aliases` AS select concat(`virtual_aliases`.`source`,'@',`virtual_domains`.`name`) AS `email`,`virtual_aliases`.`destination` AS `destination` from (`virtual_aliases` left join `virtual_domains` on((`virtual_aliases`.`domain_id` = `virtual_domains`.`id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `view_users`
+--
+
+/*!50001 DROP TABLE IF EXISTS `view_users`*/;
+/*!50001 DROP VIEW IF EXISTS `view_users`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `view_users` AS select concat(`u`.`user`,'@',`virtual_domains`.`name`) AS `email`,`u`.`password` AS `password` from (`virtual_users` `u` left join `virtual_domains` on((`u`.`domain_id` = `virtual_domains`.`id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-01-20 22:59:06
