c35bab716a108798a127c678f759342c5508c75d
bob <bob@example.com>
2013-04-06 23:18:33 +0800

    added mailserver/*

---
+++ mailserver/ubuntu.server.best.sh
@@ -0,0 +1,7 @@
+ sudo apt-get install postfix-mysql mysql-server dovecot-pop3d dovecot-imapd amav isd-new libclass-dbi-mysql-perl
+ sudo apt-get install clamav-daemon razor pyzor cpio arj zoo nomarch lzop cabextract pax lha unrar
+ Setting up clamav-daemon (0.97.7+dfsg-1ubuntu0.12.10.1) ...
+ * Clamav signatures not found in /var/lib/clamav
+ * Please retrieve them using freshclam
+ * Then run '/etc/init.d/clamav-daemon start'
+ :) OK
