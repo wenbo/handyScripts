21e276ee22c85e06415260bcd9f7866e89bc36d6
wenbo <wen_bo@kembo-net.com>
2014-11-15 17:32:10 +0800

    added linuxs/ftp

--- tool.sh
+++ tool.sh
@@ -4,3 +4,7 @@ bob@bob-Vostro-320:catalog$ pdf2htmlEX --embed cfijo --dest-dir out catalog.pdf
 coolwanglu.github.io/pdf2htmlEX/demo/chn.html
 
 http://www.appinn.com/pdftohtml/
+
+powerdesign 数据库设计工具 
+
+rails-erd 你把这个gem加入到Gemfile bundle install && bundle exec erd 
