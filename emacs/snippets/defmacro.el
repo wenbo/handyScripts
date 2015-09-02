0e1ef6da64132e6f1c6d7854d680d6e65faf16cc
wenbo <yiyun6674@hotmail.com>
2015-09-02 13:52:03 +0800

    added emacs/snippets/defmacro.el

---
+++ emacs/snippets/defmacro.el
@@ -0,0 +1,27 @@
+(defmacro inc (var)
+	(list 'setq var (list '1+ var)))
+
+(defmacro for (var from init to final do &rest body)
+	  "Execute a simple \"for\" loop.
+For example, (for i from 1 to 10 do (print i))."
+		(list 'let (list (list var init))
+					(cons 'while (cons (list '<= var final)
+														 (append body (list (list 'inc var)))))))
+
+
+
+(for i from 1 to 3 do
+		 (setq square (* i i))
+		 (princ (format "\n%d %d" i square)))
+
+→
+(let ((i 1))
+	(while (<= i 3)
+		(setq square (* i i))
+		(princ (format "%d      %d" i square))
+		(inc i)))
+
+-|1       1
+-|2       4
+-|3       9
+⇒ nil
