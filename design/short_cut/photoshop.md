e0ecbca74ede10339a05381f352e144b188df668
wenbo <yiyun6674@hotmail.com>
2016-04-11 19:19:15 +0800

    added python/install

---
+++ design/short_cut/photoshop.md
@@ -0,0 +1,6 @@
+
+v:可缩放图片
+z: zoom,  press mouse then move left/right will zoom in/zoom out
+l: 第三个 套索工具，Command+J 可抠图，保存到新的图层
+h:
+r: rotate
---
+++ python/cmd.sh
@@ -0,0 +1,4 @@
+D:/djpy> django-admin.py startproject mysite
+D:/djpy/mysite$ python manage.py startapp blog
+D:\pydj\mysite>python manage.py runserver
+
