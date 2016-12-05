2d7e8cd62806435dd609f7fbe2c11bebca935d56
yiyun6674 <yiyun6674@hotmail.com>
2016-12-05 17:35:32 +0800

    new file:   python/pip.conf

--- python/cmd.sh
+++ python/cmd.sh
@@ -4,3 +4,12 @@ D:/djpy/mysite$ python manage.py startapp blog
 D:\pydj\mysite>python manage.py runserver
 
 pip list
+python3.5 -m ensurepip
+
+cd pip-1.5.5/
+python3 setup.py install
+OK，这样就安装好pip了，
+
+下面来安装 requests吧。
+
+pip3 install requests
