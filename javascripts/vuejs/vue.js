389bef4abb54d2fc8f9d1cc02728d55894d60447
Bo Wen <b-wen@ruby-dev.jp>
2017-06-28 16:54:56 +0900

    1 つの view は 1 つのコンポーネントを使ってレンダリングされます。したがって、同じルートに対する複数の view には複数のコンポーネントが必須になります。

--- javascripts/vuejs/vue.js
+++ javascripts/vuejs/vue.js
@@ -1,1 +1,12 @@
 vue transistion
+
+Vue.component(tagName, options)
+
+
+new VueRouter
+router.push(location, onComplete?, onAbort?)
+
+https://router.vuejs.org/ja/essentials/named-views.html
+1 つの view は 1 つのコンポーネントを使ってレンダリングされます。したがって、同じルートに対する複数の view には複数のコンポーネントが必須になります。
+
+Vue template components
