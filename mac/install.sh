3ce37a856a9ea65fb308a0c267e80bf4e1cae361
文波 <bob@bunpanoMacBook-Pro.local>
2024-02-04 21:56:59 +0900

    【Mac】Google日本語入力のインストールと設定方法

--- mac/install.sh
+++ mac/install.sh
@@ -2,3 +2,12 @@ brew install pkg-config
 gem install rmagick
 /etc/apache2
 sudo apachectl  -k restart
+
+oh-my-zsh
+
+nodebrew install v16.17.0
+npm install -g yarn
+
+Google 日本語入力
+インストール完了時点では入力ソースに「Google日本語入力」が表示されません。
+
